f2a2c212f1ad9b0881547f9f2e122d7cf27ae026
Xuân Lâm <chim31102007@gmail.com>
2024-04-21 22:13:06 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,15 @@ export default function Navbar() {
     return (
         <div className='h-24 bg-slate-900 text-white text-center leading-[6rem] text-3xl'>
 
+            <div className='div-comment hidden opacity-0'>
+                <p className='comment-ribbon-footer' style={{display: 'none'}}>&#127872;&nbsp;
+                    <span className='p-footer-ribbon'>hmmmm</span>
+                    <span className='sp-footer' id='sp-footer'></span>
+                    <span className='p-footer-ribbon'>&#10024;</span>
+                    &#127872;
+                </p>
+            </div>
+
             <span id='nav-element' style={{fontSize: '28px', lineHeight: '28px'}}>
                 <a href='/'>
                     <img style={{height: '40px', lineHeight: '28px', display: 'inline', margin: '-3px 10px 0 0'}} src='/favicon.ico' alt='ChimmLofiPlaylistLogo'></img>
